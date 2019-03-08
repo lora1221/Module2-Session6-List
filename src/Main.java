bfe0b2d97fe103f8bd1670b3aa052f5c837a3ca8
lora1221 <lori.tran1221@gmail.com>
2019-03-08 09:58:28 +0700

    1st

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        MyList array = new MyList();
+        array.add(10);
+        array.add("Hue");
+        array.add(323.12);
+        array.remove(1);
+        array.get(1);
+        System.out.println();
+
+    }
+}
